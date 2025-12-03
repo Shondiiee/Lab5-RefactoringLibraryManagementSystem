802e723719580d2055b9f25530aa911d79d42f13
Daniel Cuevas <dcuevas.alarcon02@gmail.com>
2025-12-02 22:13:39 -0800

    Refactor Book.java to PaperBook.java
    Added an implementation to the interface book

--- COSC236-Lab5/src/lab5/PaperBook.java
+++ COSC236-Lab5/src/lab5/PaperBook.java
@@ -1,26 +1,35 @@
 package lab5;
 
-public class Book {
+public class PaperBook implements Book {
 	
 	private String title;
 	private boolean isAvailable;
 	
-	public Book(String title) {
+	public PaperBook(String title) {
 		this.title = title;
 		this.isAvailable = true;
 	}
+	
+	@Override
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
+	@Override
 	public String toString() {
 		return "Book: " + title;
 	}
+	
+	@Override
 	public void setIsAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
+	
+	@Override
 	public boolean getIsAvailable() {
 		return isAvailable;
 	}
